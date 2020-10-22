63da0a0f79ec8efef5e5ced35e8b8df1574fe498
Ravi Chandra Padmala <neena@nilenso.com>
2020-10-22 16:29:19 +0530

    Allow users to logout

---
+++ chronograph/Views/Components/LogoutButton.swift
@@ -0,0 +1,27 @@
+//
+//  LogoutButton.swift
+//  chronograph
+//
+//  Created by Ravi Chandra Padmala on 22/10/20.
+//  Copyright © 2020 nilenso. All rights reserved.
+//
+
+import SwiftUI
+
+struct LogoutButton: View {
+    @Environment(\.store) var store: Store
+
+    var body: some View {
+        Button("Logout", action: { () -> Void in
+            if !self.store.logout() {
+                debugPrint("Logout failed")
+            }
+        })
+    }
+}
+
+struct LogoutButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoutButton()
+    }
+}
